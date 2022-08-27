9457991de18a5d051ef6a3e195f0b826c05b647f
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-08-27 16:41:53 -0300

    added vehicle model

---
+++ src/models/Vehicle.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize');
+const db = require('./database');
+
+const Vehicle = db.define('vehicles', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  brand: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  model: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  color: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  plate: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  inventory: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  value: {
+    type: DataTypes.BIGINT,
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+});
+
+// Create table
+// Vehicle.sync({ alter: true });
+
+module.exports = Vehicle;
