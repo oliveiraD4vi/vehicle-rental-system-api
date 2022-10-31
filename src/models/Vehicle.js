31b41c9ebb15626c9b13d6a8f206c7902904db9a
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-10-31 18:20:22 -0300

    removed vehicles non needed columns

--- src/models/Vehicle.js
+++ src/models/Vehicle.js
@@ -29,14 +29,6 @@ const Vehicle = db.define('vehicles', {
     type: DataTypes.BIGINT,
     allowNull: false
   },
-  rented: {
-    type: DataTypes.DATE,
-    allowNull: true
-  },
-  devolution: {
-    type: DataTypes.DATE,
-    allowNull: true
-  },
   createdAt: {
     type: DataTypes.DATE,
     allowNull: true
